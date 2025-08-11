a1109f64b00ee6777aa993f5c0a9577415473f3e
Rodney Madondo <rodney@finfind.co.za>
2025-08-11 11:18:22 +0200

    lastest dist

--- prettier.config.mjs
+++
@@ -1,6 +0,0 @@
-import prettierPluginTailwindCSS from 'prettier-plugin-tailwindcss';
-
-/** @type {import("prettier").Config} */
-export default {
-  plugins: [prettierPluginTailwindCSS],
-};
--- src/Components/EmailLink.jsx
+++
@@ -1,9 +0,0 @@
-export function EmailLink() {
-    const user = 'madondotakundaoriginal';
-    const domain = "gmail.com";
-    const email = `${user}@${domain}`;
-    return (
-        <a href={`mailto:${email}`}
-            className="email-link rotate-90 origin-top-left z-[100] relative hover:text-[var(--text-primary)] left-[1.7rem] hover:cursor-pointer hover:translate-y-[-8px] transition-all duration-300 ease-in-out  h-8 text-[var(--text-secondary)] text-sm">{email}</a>
-    )
-}
